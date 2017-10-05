c5a79677a4b299cfc3ba55b9186a97029aff9425
carol-solivos <carol.solivos@gmail.com>
2017-10-05 11:18:01 -0400

    mi tarjeta valida

---
+++ README.md
@@ -0,0 +1,17 @@
+# Tarjeta de crédito válida
+#### Esta función permite saber si un número de tarjeta de crédito es válida o no.
+
+Primero el usuario ingresa su número de tarjeta y el programa revisa si ingresó los datos 
+pedidos que corresponden sólo a números.
+
+	Si corresponde el programa toma número a número de derecha a izquierda.
+	Los números en posición impar de derecha a izquierda los devuelve a un nuevo array.
+	Los números en posición par los multiplica, si el producto resulta en 2 dígitos 
+	suma cada dígito y lo guarda en el nuevo array. Si el producto tiene un dígito lo guarda 
+	directamente en ese mismo array.
+
+	Luego suma los digitos del array y comprueba si dividido en 10 el módulo da 0. Si es así
+	muestra al usuario que su tarjeta es válida, si no que no es válida.
+
+
+--------> ¡Buen día! <---------
---
+++ app.js
@@ -0,0 +1,39 @@
+
+function isValidCard(number){
+	do{
+		var patt = /^[0-9]*$/; // Expresión regular para que solo ingresen números.
+		var answer = prompt("Ingresa el número que quieres validar"); // Número ingresado por el usuario.
+		if(patt.test(answer) === false){
+			alert('Ingrese solo números del 0 al 9');
+		} else if(patt.test(answer) === true){
+			var array1 = []; // Array que guarda el resultado final de cada digito ingresado por el usuario.
+			var result = 0; // Es el resultado total de la suma de cada digito del número ingresado, luego de pasar por los siguientes pasos.
+			for(var i = 1; i <= answer.length; i++){
+				if (i % 2 == 0){ // Si i es par que haga las siguientes operaciones.
+					var pairProduct = parseInt(answer[answer.length - i]) * 2; // Multiplica los datos en posición par de derecha a izquierda.
+					if(pairProduct > 9){ // Si tiene dos digitos que haga lo siguiente.
+						var numToString = pairProduct.toString(); // Transforma el número de dos dígitos a string.
+						array1.push(parseInt(numToString[0]) + parseInt(numToString[1])); // Suma el primer y segundo dígito y lo guarda en el array1.
+					} else {
+						array1.push(pairProduct); //Si tiene un solo dígito lo guarda directamente en array1.
+					}
+				} else {
+					array1.push(parseInt(answer[answer.length - i])); // Si el número está en posición impar de derecha a izquierda lo guarda en array1.
+				}
+			} 	
+			for(var j = 0; j < array1.length; j++){
+				result += array1[j]; // Suma la variable result con cada número del array1.
+			} 
+			if(result % 10 == 0){ // Si la suma total dividida en 10 da módulo 0 que haga el siguiente paso.
+				return alert('Su tarjeta de crédito es válida');
+			} else { // Si la suma total no da módulo 0.
+				return alert('Su tarjeta de crédito no es válida');
+			}
+		}
+
+	} while (answer == "" || patt.test(answer) === false ); // Si no cumple la expresión regular patt o no ingresa nada que vuelva a preguntar.
+
+}
+
+isValidCard();
+//numero válido para probar = 4083952015263
